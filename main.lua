95929ebac6416ec7778ca431c2535b995e761044
allicoder <allicoder@users.noreply.github.com>
2015-03-21 00:12:27 +0000

    Clarify the licensing...
    As in, it's still GPL but if you want to use it in a commercial project you
    probably have bigger problems.

--- main.lua
+++ main.lua
@@ -18,6 +18,10 @@
 -- You should have received a copy of the GNU General Public License along with
 -- this program; if not, write to the Free Software Foundation, Inc., 51
 -- Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+--
+-- --
+--
+-- Like you're gonna wanna copy it anyway, but eh. It's the idea of the thing.
 --]]
 
 require("lfs")
